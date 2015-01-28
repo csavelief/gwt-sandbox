2df6d3e22addd75deb4e8528bd9e4014473ccee0
Cyrille Savelief <csavelief@mncc.fr>
2015-01-28 21:39:43 +0100

    Add app css

--- src/fr/mncc/sandbox/client/assets/AppCss.java
+++ src/fr/mncc/sandbox/client/assets/AppCss.java
@@ -7,5 +7,23 @@ import com.google.gwt.resources.client.CssResource;
  */
 public interface AppCss extends CssResource {
 
-    String empty();
+    String content();
+
+    @ClassName("pure-menu-heading") String pure_menu_heading();
+
+    @ClassName("content-subhead") String content_subhead();
+
+    @ClassName("pure-img-responsive") String pure_img_responsive();
+
+    @ClassName("menu-link") String menu_link();
+
+    String active();
+
+    @ClassName("menu-item-divided") String menu_item_divided();
+
+    @ClassName("pure-menu") String pure_menu();
+
+    String header();
+
+    @ClassName("pure-menu-selected") String pure_menu_selected();
 }
