1e4ed86377662f843411b59d4c73ef96f097ccc4
Cyrille Savelief <csavelief@mncc.fr>
2015-01-30 11:06:13 +0100

    Fix case sensitivity issue

---
+++ src/fr/mncc/gwttoolbox/filters/server/GwtCacheFilter.java
@@ -0,0 +1,23 @@
+package fr.mncc.gwttoolbox.filters.server;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+public class GwtCacheFilter implements Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain)
+        throws IOException, ServletException {
+       // TODO
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
