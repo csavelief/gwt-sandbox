73b5cb1e673ef89faac724eee59853d368e8877c
Cyrille Savelief <csavelief@mncc.fr>
2015-01-30 09:10:22 +0100

    Add license

--- src/fr/mncc/sandbox/client/assets/SandboxResourceBundle.java
+++ src/fr/mncc/sandbox/client/assets/SandboxResourceBundle.java
@@ -1,3 +1,23 @@
+/**
+ * Copyright (c) 2014 MNCC
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+ * associated documentation files (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ * sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or
+ * substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+ * NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ * @author http://www.mncc.fr
+ */
 package fr.mncc.sandbox.client.assets;
 
 import com.google.gwt.core.client.GWT;
@@ -10,7 +30,7 @@ public interface SandboxResourceBundle extends GwtToolboxResourceBundle {
     public static final SandboxResourceBundle INSTANCE = GWT.create(SandboxResourceBundle.class);
 
     @Source({"styles/SideMenuLayout.gss",
-        "../../../gwttoolbox/client/assets/styles/pure/0.5.0/pure.gss"}) SideMenuLayoutCss SideMenuLayoutCss();
+        "../../../gwttoolbox/client/assets/styles/pure/0.5.0/pure.gss"}) SideMenuLayoutCss sideMenuLayoutCss();
 
     @Source("images/9069037713_1752f5daeb.jpg") ImageResource img1();
 
--- src/fr/mncc/sandbox/client/layouts/sidemenu/SideMenuLayout.java
+++ src/fr/mncc/sandbox/client/layouts/sidemenu/SideMenuLayout.java
@@ -1,3 +1,23 @@
+/**
+ * Copyright (c) 2014 MNCC
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+ * associated documentation files (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ * sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or
+ * substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+ * NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ * @author http://www.mncc.fr
+ */
 package fr.mncc.sandbox.client.layouts.sidemenu;
 
 import com.google.gwt.core.client.GWT;
@@ -34,16 +54,16 @@ public class SideMenuLayout extends Composite {
         super.onLoad();
 
         // Inject layout stylesheet
-        SandboxResourceBundle.INSTANCE.SideMenuLayoutCss().ensureInjected();
+        SandboxResourceBundle.INSTANCE.sideMenuLayoutCss().ensureInjected();
 
         // Bind click event to menu button
         DOM.setEventListener(menuLink, new EventListener() {
 
             @Override public void onBrowserEvent(Event event) {
                 if (event.getTypeInt() == Event.ONCLICK) {
-                    toggleClass(layout.getElement(), SandboxResourceBundle.INSTANCE.SideMenuLayoutCss().active());
-                    toggleClass(menu, SandboxResourceBundle.INSTANCE.SideMenuLayoutCss().active());
-                    toggleClass(menuLink, SandboxResourceBundle.INSTANCE.SideMenuLayoutCss().active());
+                    toggleClass(layout.getElement(), SandboxResourceBundle.INSTANCE.sideMenuLayoutCss().active());
+                    toggleClass(menu, SandboxResourceBundle.INSTANCE.sideMenuLayoutCss().active());
+                    toggleClass(menuLink, SandboxResourceBundle.INSTANCE.sideMenuLayoutCss().active());
                     event.preventDefault();
                 }
             }
